85ae4b54de3d669600f910da182db5f32e9e714f
Andy Mockler <asmockler@users.noreply.github.com>
2016-10-14 19:19:29 -0600

    Add poetic, noble, tricky, coy, and :tropical-fish:

--- leslie_knope_compliments.py
+++ leslie_knope_compliments.py
@@ -7,9 +7,10 @@ import random
 name = input("Who are you? ")
 
 adj = ["gorgeous,", "voluptuous,", "elusive,", "spirited,", "clever,", "brilliantly radiant,", "fertile,",\
-"subtle,", "seductive,", "simple,", "wise,", "persistent,", "trembling,", "insatiable,", "purebred,"]
+"subtle,", "seductive,", "simple,", "wise,", "persistent,", "trembling,", "insatiable,", "purebred,", "poetic,",\
+"noble,", "tricky,", "coy,"]
 
 noun = ["land mammal", "baby", "baby owl", "starburst", "fox", "platypus", "rainbow", "lollipop", "horse's mane", \
-"sundial"]
+"sundial", "tropical fish"]
 
 print(name + ",", "you", random.choice(adj), random.choice(adj), random.choice(adj), random.choice(noun) + "!")
