75b58d09eb005964108e89c2ec7d2fae9c6f6985
hayleefay <hayleeham@gmail.com>
2016-12-31 15:16:31 -0600

    Use Datamuse API

--- leslie_knope_compliments.py
+++ leslie_knope_compliments.py
@@ -3,13 +3,26 @@ A geneartor for Leslie Knope-ish compliments.
 '''
 
 import random
+import requests
+import time
+random.seed(time.time())
 
 name = input("Who are you? ")
 
-adj = ["gorgeous,", "voluptuous,", "elusive,", "spirited,", "clever,", "brilliantly radiant,", "fertile,",\
-"subtle,", "seductive,", "simple,", "wise,", "persistent,", "trembling,", "insatiable,", "purebred,"]
+nouns = ["baby", "pony", "starburst", "fox", "platypus", "rainbow", "lollipop", \
+"banana", "bear", "cub", "librarian", "lightbulb", "cloud", "shrub", \
+"flower", "giraffe", "gemstone", "blanket", "sapling", "cucumber", "puppy", \
+"potato", "trash"]
 
-noun = ["land mammal", "baby", "baby owl", "starburst", "fox", "platypus", "rainbow", "lollipop", "horse's mane", \
-"sundial"]
+noun = random.choice(nouns)
 
-print(name + ",", "you", random.choice(adj), random.choice(adj), random.choice(adj), random.choice(noun) + "!")
+url = 'http://api.datamuse.com/words?rel_jjb={noun}&max=5&topics=lovely'.format(noun = noun)
+response = requests.get(url)
+adj = response.json()
+
+list_of_adj = []
+
+for word in adj:
+    list_of_adj.append(word['word'])
+
+print(name + ",", "you", ", ".join(list_of_adj), noun + "!")
